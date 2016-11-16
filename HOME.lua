211df000581f13e89a62767044f3c39616e4587f
fastyep <fast1998991@gmail.com>
2016-11-16 16:01:59 +0500

    Update HOME.lua

--- HOME.lua
+++ HOME.lua
@@ -1,21 +1,23 @@
-﻿labelStart = "Для начала работы воспользуйтесь формой ниже или используйте меню в верхней части программы."
+labelStart = "Для начала работы воспользуйтесь формой ниже или используйте меню в верхней части программы."
 labelAbout = "Программа предназначена для работы в двух режимах: проводник с возможностью шифровать файлы и архиватор зашифрованных архивов."
 labelSap = [[Security Algorythm, Private Protection From InfoRmation Exposure (Sapphire™) Защитный алгоритм, частная защита от уязвимости 
 информации - криптографический алгоритм с закрытым ключом для хранения и компакной передачи информации.]]
 this.Name = "Добро пожаловать!"
+this.Author = "LISENKO SOFT"
+
 Draw([[
 <FORM> 
 <label location="12,12" font="18">SapReader</label> 
 <button location="12,50" name="clickStart">Начало работы</button> 
-<button location="112,50">О программе</button> 
-<button location="203,50">О Sapphire™</button> 
+<button location="112,50" name="clickAbout">О программе</button> 
+<button location="203,50" name="clickSap">О Sapphire™</button> 
 
 <label location="12,80" name="mainLabel">Добро пожаловать в SapReader.</label>
 
 <textbox location="12,117" size="420,20" name="key">Введите ключ</textbox> 
 <richtextbox location="12,139" size="420,134" name="text">Введите текст</richtextbox> 
-<button location="12,275">Шифровать</button> 
-<button location="100,275">Расшифровать</button> 
+<button location="12,275" name="enc">Шифровать</button> 
+<button location="100,275" name="dec">Расшифровать</button> 
 
 
 
@@ -26,8 +28,25 @@ LISENKO SOFT 2016 год
 ]])
 
 function handleClick(sender,data)
-  Controls.clickStart.MouseUp:Remove(handler)
+if sender.Name == "clickStart" then
   Controls.mainLabel.Text = labelStart
-end
+  elseif sender.Name == "clickAbout" then
+  Controls.mainLabel.Text = labelAbout
+  else
+  Controls.mainLabel.Text = labelSap
+  end  
+  end
 
-handler=Controls.clickStart.MouseUp:Add(handleClick)
+Controls.clickStart.MouseUp:Add(handleClick)
+Controls.clickAbout.MouseUp:Add(handleClick)
+Controls.clickSap.MouseUp:Add(handleClick)
+
+function sapClick(sender,data)
+if sender.Name == "enc" then
+Controls.text.Text = Sapphire.GetCode(Controls.text.Text, Controls.key.Text)
+else
+Controls.text.Text = Sapphire.GetText(Controls.text.Text, Controls.key.Text)
+end
+end
+Controls.enc.MouseUp:Add(sapClick)
+Controls.dec.MouseUp:Add(sapClick)
